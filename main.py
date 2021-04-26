4ecec79369876ce23f8adedd091872b851f9765c
Jacob Cox <62845242+jakec0413lv@users.noreply.github.com>
2021-04-26 00:06:18 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
-from datetime import datetime
+import json, glob
+from pathlib import Path
+import random
 
 Builder.load_file('design.kv')
 
@@ -11,6 +12,14 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+    def login(self, uname, pwd):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pwd:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password!"
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pwd):
@@ -29,6 +38,23 @@ class SignUpSuccess(Screen):
     def switchToLogin(self):
         self.manager.transition.direction = 'right' #Page switch orientation
         self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def logout(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+    def showText(self, emotion):
+        emotion = emotion.lower()
+        availableEmotions = glob.glob("quotes/*txt")
+        availableEmotions = [Path(filename).stem for filename in availableEmotions]
+        if emotion in availableEmotions:
+            with open(f"quotes/{emotion}.txt", encoding="utf-8") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes) #random.choice returns randomly selected list element
+        else:
+            self.ids.quote.text = "Try another feeling..."
+
 
 class RootWidget(ScreenManager):
     pass
